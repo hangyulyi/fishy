4c1dc866a4eab6b71bfa4813c0d39950b978196d
Hangyul Yi <hangyulyi3@gmail.com>
2024-05-22 17:55:52 -0600

    add title, pause, resume functions

--- script.js
+++ script.js
@@ -12,22 +12,40 @@ const playButton = {
    y: 200,
    width: 100,
    height: 50,
-   text: "Play"
+   text: "play"
 }
 
 const howToPlayButton = {
-   x: 350,
+   x: 335,
    y: 300,
+   width: 130,
+   height: 50,
+   text: "how to play"
+}
+
+const pauseButton = {
+   x: 700,
+   y: 10,
+   width: 80,
+   height: 30,
+   text: "pause"
+}
+
+const resumeButton = {
+   x: 350,
+   y: 200,
    width: 100,
    height: 50,
-   text: "How to Play"
+   text: "resume"
 }
 
 function drawButton(button) {
    ctx.fillStyle = "white";
    ctx.fillRect(button.x, button.y, button.width, button.height);
    ctx.strokeStyle = "black";
-   ctx.font = "20px Ariel";
+   ctx.strokeRect(button.x, button.y, button.width, button.height);
+   ctx.fillStyle = "black";
+   ctx.font = "20px Arial";
    ctx.textAlign = "center";
    ctx.textBaseline = "middle";
    ctx.fillText(button.text, button.x + button.width / 2, button.y + button.height / 2);
@@ -72,7 +90,10 @@ function animate() {
    } else if (gameState === "play") {
       ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
       drawSprite(playerSprite, player.width * player.frameX, player.height * player.frameY, player.width, player.height, player.x, player.y, player.width, player.height, player.facingRight);
+      drawButton(pauseButton);
       movePlayer();
+   } else if (gameState === "pause") {
+      drawPauseScreen();
    } else if (gameState === "howToPlay") {
       document.getElementById('instructions').style.display = 'block';
    }
@@ -86,6 +107,12 @@ function drawStartScreen() {
    drawButton(howToPlayButton);
 }
 
+function drawPauseScreen() {
+   ctx.fillStyle = "rgba(0, 0, 0, 0.5)";
+   ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+   drawButton(resumeButton);
+}
+
 animate()
 
 window.addEventListener("keydown", function(e) {
@@ -96,6 +123,7 @@ window.addEventListener("keyup", function(e) {
    delete keys[e.keyCode];
 });
 
+// handle click on buttons
 canvas.addEventListener("click", function(e) {
    const rect = canvas.getBoundingClientRect();
    const mouseX = e.clientX - rect.left;
@@ -106,6 +134,14 @@ canvas.addEventListener("click", function(e) {
          gameState = "play";
       } else if (isInsideButton(mouseX, mouseY, howToPlayButton)) {
          gameState = "howToPlay";
+      }
+   } else if (gameState === "play") {
+      if (isInsideButton(mouseX, mouseY, pauseButton)) {
+         gameState = "pause";
+      }
+   } else if (gameState === "pause") {
+      if (isInsideButton(mouseX, mouseY, resumeButton)) {
+         gameState = "play";
       }
    }
 })
@@ -121,6 +157,7 @@ function isInsideButton(x, y, button) {
 }
 
 function movePlayer() {
+   if (gameState !== "play") return;
    // 
    // for up arrow
    if(keys[38]) {
